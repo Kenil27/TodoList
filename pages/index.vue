7bc4353788f07e682014759580cd9ea01fe3786b
Kenil <kenilsha93@gmail.com>
2018-11-29 15:54:18 +0530

    input field

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,46 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        todoList
-      </h1>
-      <h2 class="subtitle">
-        My flawless Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div class="container">
+    <p class="header">This is a sample Todo List</p>
+    <div class="todo">
+
+        <textarea type="text" v-model="message" class="list" name="list" required></textarea>
+        <p> {{ message }} </p> 
+    </div> 
+  </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
 
+<script>
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+    message : ' '
   }
-}
+  }
+};
 </script>
 
 <style>
-
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  height: 100vh;
+  
+}
+.header {
   text-align: center;
+  margin: 20px 20px;
+  font-size: 26px;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.todo {
+  margin :0 auto;
+  width: 50%;
+ 
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+textarea {
+  width: 90%;
+  margin: 10px;
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid #eee;
 }
 </style>
