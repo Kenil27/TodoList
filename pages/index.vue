1a82db13a57454b663605e03efcfa980ff58e49f
Kenil <kenilsha93@gmail.com>
2018-12-12 22:24:07 +0530

    Date added

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="container">
-    <h1 class="header"> My Todo List</h1>
+    <h1 class="header">My Todo List</h1>
     <div class="todo">
-        <input type="text" placeholder="+  Add a to-do..." class="list" v-model="pushItem"  @keyup.enter="pushInput" />
-        <br><br><div class="all-todos">
-          <p v-for="note in notes" :key="note.title" class="list-item">
-            <input type="checkbox" id="checkbox" class="checkmark" v-model="note.checked" v-on:change="toggleNote">
-            {{note.title}}
-            <button @click="removeItem(note)" class="btn" type="button">x</button>
-          </p> 
-        </div>
-    </div> 
+      <p class="date">{{today}}</p>
+      <br>
+      <input
+        type="text"
+        placeholder="+  Add a to-do..."
+        class="list"
+        v-model="pushItem"
+        @keyup.enter="pushInput"
+      >
+      <br>
+      <br>
+
+      <div class="all-todos">
+        <p v-for="note in notes" :key="note.title" class="list-item">
+          <input type="checkbox" id="checkbox" v-model="note.checked" v-on:change="toggleNote">
+          {{note.title}}
+          <a @click="removeItem(note)" class="close">x</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,78 +30,87 @@
 export default {
   data() {
     return {
-      pushItem: '',
-      notes : []
-    }
+      pushItem: "",
+      notes: [],
+      today: new Date().toDateString()
+    };
   },
   mounted() {
-    const str = localStorage.getItem('myTodos')
-    if (str) { 
-      const parsedArr = JSON.parse(str)
-      this.notes = parsedArr
+    const str = localStorage.getItem("myTodos");
+    if (str) {
+      const parsedArr = JSON.parse(str);
+      this.notes = parsedArr;
     } else {
-      this.notes = []
+      this.notes = [];
     }
   },
   methods: {
-    pushInput(){
-      this.notes.push({title: this.pushItem, checked: false})
-      this.pushItem= '';
-      this.saveNotesToStorage()
+    pushInput() {
+      this.notes.push({ title: this.pushItem, checked: false });
+      this.pushItem = "";
+      this.saveNotesToStorage();
     },
     toggleNote(e) {
-      this.saveNotesToStorage()
+      this.saveNotesToStorage();
     },
     removeItem(note) {
-      this.notes.splice(this.notes.indexOf(note),1)
-      this.saveNotesToStorage()
+      this.notes.splice(this.notes.indexOf(note), 1);
+      this.saveNotesToStorage();
     },
     saveNotesToStorage() {
-      localStorage.setItem('myTodos', JSON.stringify(this.notes));
+      localStorage.setItem("myTodos", JSON.stringify(this.notes));
     }
-  },
+  }
 };
 </script>
 
 <style>
 .container {
   height: 100vh;
-  background-color:cornsilk;
+  background-color: cornsilk;
 }
 .header {
   text-align: center;
   margin: 20px 20px;
   font-size: 36px;
-  font-family: 'Times New Roman', Times, serif;
+  font-family: "Times New Roman", Times, serif;
   color: blue;
 }
-
+.date {
+}
 .todo {
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
 }
-.list{
-border: 1px solid aquamarine;
-border-radius: 15px;
-height: 40px;
-padding-left: 10px;
-background-color: aquamarine;
-width: 35%;
-}
-.all-todos{
+.list {
+  border: 1px solid aquamarine;
+  border-radius: 15px;
+  height: 40px;
+  padding-left: 10px;
+  background-color: aquamarine;
   width: 35%;
 }
-.list-item{
-  background-color:white;
+.all-todos {
+  width: 35%;
+}
+.list-item {
+  background-color: white;
   margin-bottom: 10px;
   height: 30px;
 }
-.btn{
-  
+#checkbox {
+  width: 15px;
+  height: 15px;
+}
+.close {
+  opacity: 0.4;
   float: right;
-  background: transparent;
-  
+  margin-right: 10px;
+  font-size: 18px;
+}
+.close:hover {
+  opacity: 1;
+  cursor: pointer;
 }
 </style>
